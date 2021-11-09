fd5c3b4dd5d74630a419864f0c0c7297425805ec
AlexSin313 <93577326+AlexSin313@users.noreply.github.com>
2021-11-09 18:08:52 +0800

    Add files via upload

---
+++ printBigU.py
@@ -0,0 +1,14 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:\Users\ywsin9\Downloads\printBigA.py ===============
+
+    U          U
+    U          U
+    U          U
+    U          U
+    UU        UU
+     UUU    UUU  
+        UUUU     
+
+>>> 
